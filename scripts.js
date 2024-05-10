576b7f41fa5070d11660f736d31bb5563eab2a9c
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-05-10 10:06:55 +0200

    Updated documentation

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-
+/**
+ * @constant {number}- decalred a constat varibale for calculations
+ */
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  const calculations = dividend / divider; //decalred a variable to keep track of calulations
+
+  result.innerText = Math.floor(calculations);
 });
