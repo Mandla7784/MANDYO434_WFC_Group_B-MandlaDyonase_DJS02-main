7bb7d5ff1e417cddc745560b72f43b8cdeb85a07
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-05-10 10:39:20 +0200

    Updated script file

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+
 /**
  * @constant {number}- decalred a constat varibale for calculations
+ *
  */
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  try {
+    if (!dividend || !divider) {
+      result.innerText = `Division not performed. Both values are required in inputs. Try again`;
+      throw ReferenceError(
+        "Division not performed. Both values are required in inputs. Try again"
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
   const calculations = dividend / divider; //decalred a variable to keep track of calulations
 
-  result.innerText = Math.floor(calculations);
+  result.innerText = Math.floor(calculations); //user story 2
 });
