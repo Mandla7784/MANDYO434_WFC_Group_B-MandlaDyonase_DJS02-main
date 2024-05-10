6102ae1938f6669d4c8c45c3d11aa9141693b54c
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-05-10 11:01:20 +0200

    Added js docs

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ const result = document.querySelector("[data-result]");
 
 /**
  * @constant {number}- decalred a constat varibale for calculations
+ * @throws {Error} -if values are not provided then will throw the error
+ * @throws {Error}- if the values are not valid integer
  *
  */
 form.addEventListener("submit", (event) => {
@@ -12,18 +14,22 @@ form.addEventListener("submit", (event) => {
 
   try {
     if (!dividend || !divider) {
+      //if no value provided the the message will be displayed
       result.innerText = `Division not performed. Both values are required in inputs. Try again`;
       throw ReferenceError(
         "Division not performed. Both values are required in inputs. Try again"
       );
+    } else if (typeof dividend !== "number" || typeof divider !== "number") {
+      document.body.innerHTML = `<h1>Something critical went wrong. Please reload the page</h1>`;
+      throw ReferenceError(
+        "Something critical went wrong. Please reload the page"
+      );
     } else {
       const calculations = dividend / divider; //decalred a variable to keep track of calulations
 
-      result.innerText = Math.floor(calculations);
+      result.innerText = Math.floor(calculations); //user story 2...showing the results on the DOM
     }
   } catch (error) {
     console.error(error);
   }
-
-  //user story 2
 });
